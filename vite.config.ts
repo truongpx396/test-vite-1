89959161ba38f8b54a2592f38045c85b11c9b1a0
truongpx-Mac <truongpx396@gmail.com>
2024-11-11 14:35:54 +0700

    Update CI/CD workflow permissions and set base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/test-vite-1/', // Replace with your repository name
   plugins: [react()],
   server: {
     port: 3000,
